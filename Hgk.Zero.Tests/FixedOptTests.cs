245132e0216fcf93d0e6621d5315f1202bf9e128
Peter S. May <me@psmay.com>
2019-01-04 00:51:50 -0500

    Slowly growing the unit tests

--- Hgk.Zero.Tests/FixedOptTests.cs
+++ Hgk.Zero.Tests/FixedOptTests.cs
@@ -8,25 +8,91 @@ namespace Hgk.Zero.Tests
     public class FixedOptTests
     {
         [Fact]
-        public void EmptyOptIsEmpty()
+        public void EmptyOptOfIntIsEmpty()
         {
-            var opt = Opt.Empty<string>();
-            Assert.False(opt.HasValue);
-            Assert.Throws<InvalidOperationException>(() => opt.Value);
-            Assert.False(opt.TryGetValue(out string retrievedValue));
-            Assert.Equal(Enumerable.Empty<string>(), opt);
+            var opt = Opt.Empty<int>();
+            AssertEmpty(opt);
         }
 
         [Fact]
-        public void FullOptIsFull()
+        public void EmptyOptOfObjectIsEmpty()
         {
-            var value = "hello";
+            var opt = Opt.Empty<object>();
+            AssertEmpty(opt);
+        }
+
+        [Fact]
+        public void FullOptOfIntIsFull()
+        {
+            var value = 12345;
             var opt = Opt.Full(value);
+            AssertContainsValue(opt, 12345);
+        }
+
+        [Fact]
+        public void FullOptOfObjectIsFull()
+        {
+            var value = new object();
+            var opt = Opt.Full(value);
+            AssertContainsObject(opt, value);
+        }
+
+        [Fact]
+        public void CreateOptOfNullIntIsEmpty()
+        {
+            int? value = null;
+            var opt = Opt.Create(value);
+            AssertEmpty(opt);
+        }
+
+        [Fact]
+        public void CreateOptOfNonNullNullableIntIsFull()
+        {
+            int? value = 12345;
+            var opt = Opt.Create(value);
+            AssertContainsValue(opt, value.Value);
+        }
+
+        [Fact]
+        public void CreateOptOfNullObjectIsEmpty()
+        {
+            object value = null;
+            var opt = Opt.Create(value);
+            AssertEmpty(opt);
+        }
+
+        [Fact]
+        public void CreateOptOfNonNullObjectIsFull()
+        {
+            object value = new object();
+            var opt = Opt.Create(value);
+            AssertContainsObject(opt, value);
+        }
+
+        private static void AssertContainsObject<T>(Opt<T> opt, T value) where T : class
+        {
             Assert.True(opt.HasValue);
             Assert.Same(value, opt.Value);
-            Assert.True(opt.TryGetValue(out string retrievedValue));
+            Assert.True(opt.TryGetValue(out T retrievedValue));
             Assert.Same(value, retrievedValue);
-            Assert.Equal(new[] { value }, opt);
+            Assert.Equal(new T[] { value }, opt);
+        }
+
+        private static void AssertContainsValue<T>(Opt<T> opt, T value) where T : struct
+        {
+            Assert.True(opt.HasValue);
+            Assert.Equal(value, opt.Value);
+            Assert.True(opt.TryGetValue(out T retrievedValue));
+            Assert.Equal(value, retrievedValue);
+            Assert.Equal(new T[] { value }, opt);
+        }
+
+        private static void AssertEmpty<T>(Opt<T> opt)
+        {
+            Assert.False(opt.HasValue);
+            Assert.Throws<InvalidOperationException>(() => opt.Value);
+            Assert.False(opt.TryGetValue(out T retrievedValue));
+            Assert.Equal(Enumerable.Empty<T>(), opt);
         }
     }
 }
