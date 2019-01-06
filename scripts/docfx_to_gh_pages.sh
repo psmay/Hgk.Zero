a1fe02898fc0af89ba5c9ceaf556b7a5d189724a
Peter S. May <me@psmay.com>
2019-01-06 17:59:23 -0500

    Try reeling it back to mono-runtime from mono-complete

--- scripts/docfx_to_gh_pages.sh
+++ scripts/docfx_to_gh_pages.sh
@@ -18,7 +18,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 sudo apt install apt-transport-https
 echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt-get install mono-complete nuget &&
+sudo apt-get install mono-runtime nuget &&
 
 # Install docfx.console
 nuget install docfx.console -ExcludeVersion &&
