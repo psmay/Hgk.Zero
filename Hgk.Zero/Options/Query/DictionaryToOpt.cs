92c27f21c46e418bb6849353f48705b1765a46eb
psmay <me@psmay.com>
2019-01-07 20:47:32 -0500

    Massive unbreaks (#21)
    Addressing problems found while writing examples
    
    *   DictionaryToOpt: Several framework dictionary types implement both
        IDictionary<T> and IReadOnlyDictionary<T>, which causes an ambiguity
        when using GetValueOpt(). This is addressed (currently) by adding
        specific implementations for each of the framework types affected.
        For now, everyone else will need to cast to IDictionary<T> or
        IReadOnlyDictionary<T>.
    *   EnumerableToOpt: WhereSingle() with predicate returned IOpt<T>
        instead of ISingleResultOpt<T>.
    *   AbstractSingleResultOpt: ToString() is now implemented.
    *   Opt: ReplaceIfEmpty() is now implemented.
    *   SingleResultOpt:
        *   DefaultIfMoreThanOne() is now implemented.
        *   ReplaceIfMoreThanOne() now accepts a replacement of
            Func<IOpt<T>> instead of only Func<Opt<T>>.
    *   Strings: Names of methods changed to disambiguate from an improper
        use of e.g. string.Join. An extension method on string named Join
        apparently refuses to work as a special case. Thanks, framework
        designers. [grumbles]

--- Hgk.Zero/Options/Query/DictionaryToOpt.cs
+++ Hgk.Zero/Options/Query/DictionaryToOpt.cs
@@ -1,6 +1,9 @@
 ﻿using Hgk.Zero.Options.Try;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 
 namespace Hgk.Zero.Options.Query
 {
@@ -48,5 +51,160 @@ namespace Hgk.Zero.Options.Query
             if (source == null) throw new ArgumentNullException(nameof(source));
             return TryToOpt<TValue>.Call(source.TryGetValue, key);
         }
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this Dictionary<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this SortedDictionary<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this SortedList<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this ImmutableDictionary<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this ImmutableDictionary<TKey, TValue>.Builder source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this ImmutableSortedDictionary<TKey, TValue>.Builder source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this ImmutableSortedDictionary<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        /// <summary>
+        /// Gets the value associated with a specified key as an option.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A source dictionary.</param>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>
+        /// An option containing the value associated with <paramref name="key"/> in <paramref
+        /// name="source"/>, if it is found; otherwise, an empty option.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="key"/> is <see langword="null"/>.
+        /// </exception>
+        public static Opt<TValue> GetValueOpt<TKey, TValue>(this ReadOnlyDictionary<TKey, TValue> source, TKey key) =>
+            source.AsReadOnlyDictionary().GetValueOpt(key);
+
+        private static IReadOnlyDictionary<TKey, TValue> AsReadOnlyDictionary<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source) => source;
     }
 }
